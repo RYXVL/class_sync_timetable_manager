e53528fb65d6b545e7ce397428250e323a4b9c54
RYXVL <sojan.ryan@gmail.com>
2024-03-02 16:28:59 +0530

    rendered dummy data on timetable screen

---
+++ lib/dummy_data.dart
@@ -0,0 +1,474 @@
+import 'package:flutter/material.dart';
+
+class DummyData {
+  List dummyTimetable = [
+    {
+      "dayOfWeek": "Monday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+    {
+      "dayOfWeek": "Tuesday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+    {
+      "dayOfWeek": "Wednesday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+    {
+      "dayOfWeek": "Thursday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+    {
+      "dayOfWeek": "Friday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+    {
+      "dayOfWeek": "Saturday",
+      "timetable": [
+        {
+          "timeStart": "08:00am",
+          "timeEnd": "09:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "09:00am",
+          "timeEnd": "10:00am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:00am",
+          "timeEnd": "10:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "10:30am",
+          "timeEnd": "11:30am",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "11:30am",
+          "timeEnd": "12:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "12:30pm",
+          "timeEnd": "01:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "01:00pm",
+          "timeEnd": "02:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "02:00pm",
+          "timeEnd": "03:00pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:00pm",
+          "timeEnd": "03:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+        {
+          "timeStart": "03:30pm",
+          "timeEnd": "04:30pm",
+          "subjectName": "testsubject",
+          "semester": "testsemester",
+          "section": "testsection",
+        },
+      ],
+    },
+  ];
+
+  List<Widget> generateWidgetsForDummyData() {
+    List<Widget> generatedList = [];
+    for (var daysTimeTable in dummyTimetable) {
+      generatedList.add(Text(daysTimeTable['dayOfWeek']));
+      List timetable = daysTimeTable['timetable'];
+      for (var slot in timetable) {
+        String timeStart = slot['timeStart'];
+        String timeEnd = slot['timeEnd'];
+        String subjectName = slot['subjectName'];
+        String semester = slot['semester'];
+        String section = slot['section'];
+        generatedList.add(
+            Text('$timeStart - $timeEnd: $subjectName -> $semester-$section'));
+      }
+    }
+    return generatedList;
+  }
+}
--- lib/screens/timetable_screen.dart
+++ lib/screens/timetable_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../dummy_data.dart';
+
 class TimetableScreen extends StatefulWidget {
   const TimetableScreen({Key? key}) : super(key: key);
 
@@ -8,11 +10,18 @@ class TimetableScreen extends StatefulWidget {
 }
 
 class _TimetableScreenState extends State<TimetableScreen> {
+  DummyData dummyData = DummyData();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Timetable Screen'),
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: dummyData.generateWidgetsForDummyData(),
+        ),
       ),
     );
   }
